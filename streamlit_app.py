f8a6eb72a7afe9c4e7a93b8d31d968a440be29e7
BrahmeshGitHub <31551617+BrahmeshGitHub@users.noreply.github.com>
2023-06-19 12:02:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlist.text('Kale, Spinach and Rocket Smothie')
+streamlit.text('Hard Boiled and Free Range Egg')

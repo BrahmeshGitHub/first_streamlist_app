8b22fd826aa7f3025e965d0080b93102d32c49d3
BrahmeshGitHub <31551617+BrahmeshGitHub@users.noreply.github.com>
2023-06-19 12:07:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach and Rocket Smothie')
 streamlit.text('🐔 Hard Boiled and Free Range Egg')
-steamlit.text('🥑🍞 Avocado Toast')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
